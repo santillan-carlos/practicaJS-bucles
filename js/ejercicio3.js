f8d9933013c42f546c7d63313683b2bc029d528d
carlos santillan <carlos_sks@hotmail.es>
2021-02-22 22:06:53 -0300

    se comenzo a resolver el ejercicio 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,6 @@
+//Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión 
+let palabra = prompt("Ingrese una palabra");
+if(palabra = true){
+    document.write(palabra + "");
+    
+}
